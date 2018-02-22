e00945190cbb12749731a62f3c8f9f32029ceb4f
Marshall <marshallfrink@gmail.com>
2018-02-22 15:47:42 -0800

    Console ReadLine WriteLine

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFoundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            var userEntry = Console.ReadLine();
+            Console.WriteLine("Hello " + userEntry);
+        }
+    }
+}
